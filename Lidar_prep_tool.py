35ac418fd095121d62c53ea0cd7ae004e6fc294c
Eliza Wallace <ewallace@mapc.org>
2017-04-06 11:42:31 -0400

    Lidar Prep Tool
    Now handles exceptions.

--- Lidar_prep_tool.py
+++ Lidar_prep_tool.py
@@ -2,12 +2,13 @@
 # Name:        Lidar Mosaic Tool
 # Purpose:      To mosaic several lidar images
 #
-# Author:      ewallace
+# Author:      Eliza Wallace, GIS Analyst, MAPC, ewallace@mapc.org
 #
 # Created:     09/11/2014
 #-------------------------------------------------------------------------------
 
 import arcpy
+import sys
 from arcpy import env
 from arcpy.sa import *
 
@@ -16,41 +17,45 @@ outrastername = arcpy.GetParameterAsText(1)
 
 env.workspace = in_workspace
 
-lidarlist = arcpy.ListRasters()
+try:
+    lidarlist = arcpy.ListRasters()
 
-arcpy.AddMessage("Setting null values...")
+    arcpy.AddMessage("Setting null values...")
 
-for file in lidarlist:
-    outSetNull = arcpy.sa.SetNull(file, file, "VALUE < -400")
-    outsplit = file.split(".")
-    outfilename = outsplit[0] + "_stnull" + ".tif"
-    outfilepath = in_workspace + "/" + outfilename
-    outSetNull.save(outfilepath)
+    for file in lidarlist:
+       outSetNull = arcpy.sa.SetNull(file, file, "VALUE < -400")
+       outsplit = file.split(".")
+       outfilename = outsplit[0] + "_stnull" + ".tif"
+       outfilepath = in_workspace + "/" + outfilename
+       outSetNull.save(outfilepath)
 
-arcpy.AddMessage("Building pyramids and calculating statistics...")
+    arcpy.AddMessage("Building pyramids and calculating statistics...")
 
-arcpy.BuildPyramidsandStatistics_management(in_workspace, "NONE",
-"BUILD_PYRAMIDS", "CALCULATE_STATISTICS" )
+    arcpy.BuildPyramidsandStatistics_management(in_workspace, "NONE","BUILD_PYRAMIDS", "CALCULATE_STATISTICS" )
 
-lidarstnull = arcpy.ListRasters("*_stnull.tif")
+    lidarstnull = arcpy.ListRasters("*_stnull.tif")
 
-outraster = outrastername + ".tif"
+    outraster = outrastername + ".tif"
 
-arcpy.AddMessage("Mosaicking images...")
+    arcpy.AddMessage("Mosaicking images...")
 
-testraster = in_workspace + "/" + lidarstnull[0]
+    testraster = in_workspace + "/" + lidarstnull[0]
 
-pixelcode = arcpy.GetRasterProperties_management(testraster,"VALUETYPE")
+    pixelcode = arcpy.GetRasterProperties_management(testraster,"VALUETYPE") # gets value type from input lidar
 
-pixeldict = {'0':'1_BIT', '1':'2_BIT', '2':'4_BIT', '3':'8_BIT_UNSIGNED', '4':'8_BIT_SIGNED', 
-             '5':'16_BIT_UNSIGNED', '6':'16_BIT_SIGNED','7':'32_BIT_UNSIGNED','8':'32_BIT_SIGNED',
-             '9':'32_BIT_FLOAT','10':'64_BIT'}
+    pixeldict = {'0':'1_BIT', '1':'2_BIT', '2':'4_BIT', '3':'8_BIT_UNSIGNED', '4':'8_BIT_SIGNED', 
+    '5':'16_BIT_UNSIGNED', '6':'16_BIT_SIGNED','7':'32_BIT_UNSIGNED','8':'32_BIT_SIGNED',
+    '9':'32_BIT_FLOAT','10':'64_BIT'}
 
-pixelcode = str(pixelcode)
+    pixelcode = str(pixelcode)
 
-pixeltype = pixeldict.get(pixelcode)
+    pixeltype = pixeldict.get(pixelcode)
 
-arcpy.MosaicToNewRaster_management(lidarstnull, in_workspace, outraster, "",pixeltype,"","1","FIRST", "FIRST")
+    arcpy.MosaicToNewRaster_management(lidarstnull, in_workspace, outraster, "",pixeltype,"","1","FIRST", "FIRST")
 
-arcpy.AddMessage("Mosaicked to new raster.")
+    arcpy.AddMessage("Mosaicked to new raster.")
 
+except Exception:
+    e = sys.exc_info()[1]
+    print(e.args[0])
+    arcpy.AddError(e.args[0])
