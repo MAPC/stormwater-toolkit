8383e394783d63ccd17f974e598f718c32a3ffcd
Eliza Wallace <ewallace@mapc.org>
2017-04-20 14:36:32 -0400

    Lidar Mosaic Tool
    reads pixel size from a raster in the folder

--- Lidar_prep_tool.py
+++ Lidar_prep_tool.py
@@ -17,13 +17,16 @@ outrastername = arcpy.GetParameterAsText(1)
 
 env.workspace = in_workspace
 
+
+
 try:
     lidarlist = arcpy.ListRasters()
 
     arcpy.AddMessage("Setting null values...")
 
     for file in lidarlist:
-       outSetNull = arcpy.sa.SetNull(file, file, "VALUE < -400")
+       outSetNull = arcpy.sa.SetNull(file, file, "VALUE < -1355")
+       outSetNull = arcpy.sa.SetNull(file,file, "VALUE > 29100")
        outsplit = file.split(".")
        outfilename = outsplit[0] + "_stnull" + ".tif"
        outfilepath = in_workspace + "/" + outfilename
@@ -33,25 +36,26 @@ try:
 
     arcpy.BuildPyramidsandStatistics_management(in_workspace, "NONE","BUILD_PYRAMIDS", "CALCULATE_STATISTICS" )
 
-    lidarstnull = arcpy.ListRasters("*_stnull.tif")
-
+    lidarstnull = arcpy.ListRasters("*_stnull.tif") # creates a list of rasters in the folder with _stnull suffix
     outraster = outrastername + ".tif"
 
     arcpy.AddMessage("Mosaicking images...")
 
-    testraster = in_workspace + "/" + lidarstnull[0]
-
-    pixelcode = arcpy.GetRasterProperties_management(testraster,"VALUETYPE") # gets value type from input lidar
-
+    testraster = in_workspace + "/" + lidarstnull[0] #chooses a raster to check for cell size and pixel type
+    
+    #reads pixel size from the test raster and stores as text for mosaic to new raster tool
+    xcellsize = str(arcpy.GetRasterProperties_management(testraster,"CELLSIZEX"))
+    ycellsize = str(arcpy.GetRasterProperties_management(testraster,"CELLSIZEY"))
+    cellsize = str(max(int(xcellsize), int(ycellsize)))
+    
+    #reads pixel type from the test raster and stores as text for mosaic to new raster tool
     pixeldict = {'0':'1_BIT', '1':'2_BIT', '2':'4_BIT', '3':'8_BIT_UNSIGNED', '4':'8_BIT_SIGNED', 
     '5':'16_BIT_UNSIGNED', '6':'16_BIT_SIGNED','7':'32_BIT_UNSIGNED','8':'32_BIT_SIGNED',
     '9':'32_BIT_FLOAT','10':'64_BIT'}
-
-    pixelcode = str(pixelcode)
-
+    pixelcode = str(arcpy.GetRasterProperties_management(testraster,"VALUETYPE"))
     pixeltype = pixeldict.get(pixelcode)
-
-    arcpy.MosaicToNewRaster_management(lidarstnull, in_workspace, outraster, "",pixeltype,"","1","FIRST", "FIRST")
+    
+    arcpy.MosaicToNewRaster_management(lidarstnull, in_workspace, outraster, "",pixeltype,"",cellsize,"FIRST", "FIRST")
 
     arcpy.AddMessage("Mosaicked to new raster.")
 
